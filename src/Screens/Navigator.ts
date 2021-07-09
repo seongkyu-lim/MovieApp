df4fd9db273c31a59d825ba98702487cb02313b7
limseongkyu <96x60812@naver.com>
2021-07-09 21:46:35 +0900

    [Add] navigator, input, button components

---
+++ src/Screens/Navigator.ts
@@ -0,0 +1,31 @@
+import {
+	createSwitchNavigator,
+	createStackNavigator,
+	createAppContainer,
+} from 'react-navigation'
+
+import CheckLogin from '~/Screens/CheckLogin';
+import Login from '~/Screens/Login';
+import MovieHome from '~/Screens/MovieHome';
+import MovieDetail from '~/Screens/MovieDetail';
+
+const LoginNavigator = createStackNavigator({
+	Login,
+});
+
+const MovieNavigator = createStackNavigator({
+	MovieDetail,
+	MovieHome,
+});
+
+const AppNavigator = createSwitchNavigator({
+	CheckLogin,
+	LoginNavigator,
+	MovieNavigator,
+},
+	{
+		initialRouteName: 'CheckLogin',
+	}
+);
+
+export default createAppContainer(AppNavigator);
